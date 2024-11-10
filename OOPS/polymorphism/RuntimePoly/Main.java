e094ed020cd3305f542eefeafb49b81d0b45f441
Insha <inshaaa72@gmail.com>
2024-11-10 14:48:25 +0530

    14th

--- OOPS/polymorphism/RuntimePoly/Main.java
+++ OOPS/polymorphism/RuntimePoly/Main.java
@@ -12,6 +12,14 @@ public class Main {
         circle.draw();
         square.draw();
         triangle.draw();
+
+        Shapes f1 = new Circle(); // Upcasting
+        f1.draw(); // Calls the draw method of Circle class , this is called overriding
+        // this will not give error as we are upcasting Circle object to Shapes object
+
+         // Circle c1 = new Shapes(); // what happens if we do this?
+        // This will give an error as we are trying to downcast a Shapes object to Circle object
+
     }
     
 }
